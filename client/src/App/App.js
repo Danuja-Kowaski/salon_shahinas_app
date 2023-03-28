0f1ed4b63c4303d4749c8275e7aa5e1c927370bb
Supun Kalhara <gl.supunkalhara@gmail.com>
2023-03-28 20:18:00 +0530

    added home/booking page and navbar

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -4,6 +4,14 @@ import { ConfigProvider } from "antd";
 import LoginPage from "./Pages/LoginPage";
 import RegisterPage from "./Pages/RegisterPage";
 import HomePage from "./Pages/HomePage";
+import BookingPage from "./Pages/BookingPage";
+import {
+    MailOutlined,
+    BellOutlined,
+    HomeOutlined,
+    ScheduleOutlined,
+    UserOutlined,
+} from "@ant-design/icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./styles.scss";
@@ -19,7 +27,25 @@ function App() {
         >
             {/* <LoginPage /> */}
             {/* <RegisterPage /> */}
-            <HomePage />
+            {/* <HomePage /> */}
+            <BookingPage />
+            <div className="navbar">
+                <div className="nav-item">
+                    <MailOutlined />
+                </div>
+                <div className="nav-item">
+                    <BellOutlined />
+                </div>
+                <div className="nav-item active">
+                    <HomeOutlined />
+                </div>
+                <div className="nav-item">
+                    <ScheduleOutlined />
+                </div>
+                <div className="nav-item">
+                    <UserOutlined />
+                </div>
+            </div>
         </ConfigProvider>
     );
 }
